800cc9be354ff60bcfce75831974f02e93e2195d
Jake <jh@thesilverlogic.com>
2014-02-06 23:22:32 -0500

    Fix SQL injection vulnerability

--- code/filever.php
+++ code/filever.php
@@ -7,6 +7,7 @@ $input   = $_GET["n"];
 
 mysql_connect('localhost', $dbuser, $dbpass);
 @mysql_select_db($dbname) or die("Unable to select database");
+$version = mysql_real_escape_string($version);
 $input = mysql_real_escape_string($input);
 $query = mysql_query("SELECT `filename` FROM `$dbname`.`files`  WHERE name=\"$input\" AND version=\"$version\" ");
 while ($row = mysql_fetch_assoc($query)) {
